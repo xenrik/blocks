ae3351572ebc537c6965a991f253c7399391e07a
xenrik <lee@xenrik.com>
2018-07-08 09:34:25 +0100

    Setup scenes for test area and main menu Added editor script to auto switch to main menu on launch Moved scene files into the same folder for easier access

---
+++ Assets/Scripts/Simple/GotoScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GotoScene : MonoBehaviour {
+    public string SceneName;
+
+    public void Execute() {
+        SceneManager.LoadScene(SceneName);
+    }
+}
