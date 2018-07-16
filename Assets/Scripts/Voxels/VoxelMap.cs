1310c6d7a1f19c53ccf5897b981600701ae4e8d0
xenrik <lee@xenrik.com>
2018-07-16 09:02:53 +0100

    Trying to get serialization of voxelmaps working

---
+++ Assets/Scripts/Voxels/VoxelMap.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+
+/**
+ * An object that is used to describe the map for a voxel based object.
+ * It only describes the id of each voxel at a given position, relative to the
+ * origin of the object.
+ */
+public class VoxelMap : ScriptableObject {
+
+    public int this[Vector3 position] {
+        get {
+            int voxelId;
+            if (voxelMap.TryGetValue(position, out voxelId)) {
+                return voxelId;
+            } else {
+                return -1;
+            }
+        }
+
+        set {
+            voxelMap[position] = value;
+        }
+    }
+
+    /** The number of voxels in the map */
+    public int Count {
+        get {
+            return voxelMap.Count;
+        }
+    }
+
+    // The data for this map when serialized
+    [SerializeField]
+    public string Data {
+        get {
+            return SerializeMap();
+        }
+        set {
+            DeserializeMap(value);
+        }
+    }
+
+    // The map of voxels for this object.
+    private Dictionary<Vector3, int> voxelMap = new Dictionary<Vector3, int>();
+
+    /**
+     * Build the serialized form of the map
+     */
+    private string SerializeMap() {
+        Debug.Log("Serializing...");
+
+        MemoryStream buffer = new MemoryStream();
+        byte[] bytes;
+        foreach (var mapEntry in voxelMap) {
+            bytes = BitConverter.GetBytes(mapEntry.Key.x); buffer.Write(bytes, 0, bytes.Length);
+            bytes = BitConverter.GetBytes(mapEntry.Key.y); buffer.Write(bytes, 0, bytes.Length);
+            bytes = BitConverter.GetBytes(mapEntry.Key.z); buffer.Write(bytes, 0, bytes.Length);
+
+            bytes = BitConverter.GetBytes(mapEntry.Value); buffer.Write(bytes, 0, bytes.Length);
+        }
+
+        bytes = buffer.ToArray();
+        return Convert.ToBase64String(bytes);
+    }
+
+    /**
+     * Restore the map from its serialized form
+     */
+    private void DeserializeMap(string dataString) {
+        voxelMap.Clear();
+        byte[] data = Convert.FromBase64String(dataString);
+        int bytePos = 0;
+        while (bytePos < data.Length) {
+            float x = BitConverter.ToSingle(data, bytePos); bytePos += sizeof(float);
+            float y = BitConverter.ToSingle(data, bytePos); bytePos += sizeof(float);
+            float z = BitConverter.ToSingle(data, bytePos); bytePos += sizeof(float);
+
+            int voxelId = BitConverter.ToInt32(data, bytePos); bytePos += sizeof(int);
+
+            Vector3 position = new Vector3(x, y, z);
+            voxelMap[position] = voxelId;
+        }
+    }
+
+}
---
+++ Assets/Tests/VoxelMapTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class VoxelMapTest {
+
+    private VoxelMap map;
+
+    [SetUp]
+    public void SetUp() {
+        map = ScriptableObject.CreateInstance<VoxelMap>();
+    }
+
+    [Test]
+    public void TestSerializationDeserialization() {
+        // Use the Assert class to test conditions.
+        map[new Vector3(0, 0, 0)] = 0;
+        map[new Vector3(1, 0, 0)] = 1;
+        map[new Vector3(0, 1, 0)] = 2;
+        map[new Vector3(0, 0, 1)] = 3;
+        map[new Vector3(1, 1, 1)] = 4;
+
+        string json = JsonUtility.ToJson(map);
+        Debug.Log($"JSON: {json}");
+
+        VoxelMap newMap = ScriptableObject.CreateInstance<VoxelMap>();
+        JsonUtility.FromJsonOverwrite(json, newMap);
+
+        Assert.AreEqual(map.Count, newMap.Count);
+        Assert.AreEqual(map[new Vector3(0, 0, 0)], newMap[new Vector3(0, 0, 0)]);
+        Assert.AreEqual(map[new Vector3(1, 0, 0)], newMap[new Vector3(1, 0, 0)]);
+        Assert.AreEqual(map[new Vector3(0, 1, 0)], newMap[new Vector3(0, 1, 0)]);
+        Assert.AreEqual(map[new Vector3(0, 0, 1)], newMap[new Vector3(0, 0, 1)]);
+        Assert.AreEqual(map[new Vector3(1, 1, 1)], newMap[new Vector3(1, 1, 1)]);
+
+        string newJson = JsonUtility.ToJson(newMap);
+        Assert.AreEqual(json, newJson);
+    }
+}
