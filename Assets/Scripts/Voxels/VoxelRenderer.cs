f135085ddb2ae809ed5ec59697a9de160f303420
xenrik <lee@xenrik.com>
2018-07-19 08:54:32 +0100

    Tried some alternative techniques, but voxel rendering is still too slow. Think I need to change the generator so voxels are always on a integer point which will then let me use a 3d bitmap to work out where faces are needed.

--- Assets/Scripts/Voxels/VoxelRenderer.cs
+++ Assets/Scripts/Voxels/VoxelRenderer.cs
@@ -24,10 +24,10 @@ public class VoxelRenderer : MonoBehaviour {
     private float halfScale;
 
     // Use this for initialization
-    void Start () {
-       //meshGameObject = new GameObject("VoxelMesh");
-       //meshFilter = meshGameObject.AddComponent<MeshFilter>();
-       //meshRenderer = meshGameObject.AddComponent<MeshRenderer>();
+    void Start() {
+        //meshGameObject = new GameObject("VoxelMesh");
+        //meshFilter = meshGameObject.AddComponent<MeshFilter>();
+        //meshRenderer = meshGameObject.AddComponent<MeshRenderer>();
 
         meshRenderer = GetComponent<MeshRenderer>();
         meshRenderer.enabled = false;
@@ -37,15 +37,15 @@ public class VoxelRenderer : MonoBehaviour {
         voxelMap = new VoxelMap();
         voxelMap.FromJson(VoxelMapData.text);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         if (mapHashCode != voxelMap.GetHashCode()) {
             mapHashCode = voxelMap.GetHashCode();
 
             StartCoroutine(BuildMesh());
         }
-	}
+    }
 
     private IEnumerator BuildMesh() {
         Stopwatch timer = new Stopwatch();
@@ -61,11 +61,12 @@ public class VoxelRenderer : MonoBehaviour {
         // 2 - Counter Clockwise face
         // 3 - Both (i.e. not needed)
 
-        ConcurrentDictionary<Vector3,Face> faces = new ConcurrentDictionary<Vector3,Face>();
         ProgressMonitor.Begin("Building Mesh");
         ProgressMonitor.BeginSubtask("Generating Faces", voxelMap.Count);
         var directions = System.Enum.GetValues(typeof(FaceDirection));
+
         /*
+        ConcurrentDictionary<Vector3,Face> faces = new ConcurrentDictionary<Vector3,Face>();
         var options = new ParallelOptions();
         options.MaxDegreeOfParallelism = 4;
         var task = Task.Factory.StartNew(() => {
@@ -83,6 +84,133 @@ public class VoxelRenderer : MonoBehaviour {
         } 
         */
 
+        int[] min = { int.MaxValue, int.MaxValue, int.MaxValue };
+        int[] max = { int.MinValue, int.MinValue,  int.MinValue };
+        foreach (var entry in voxelMap) {
+            if (NeedsYield(yieldTimer)) {
+                yield return null;
+            }
+
+            Vector3 origin = entry.Key;
+
+            foreach (FaceDirection face in directions) {
+                switch (face) {
+                case FaceDirection.TOP: origin.y -= halfScale; break;
+                case FaceDirection.BOTTOM: origin.y += halfScale; break;
+                case FaceDirection.LEFT: origin.x -= halfScale; break;
+                case FaceDirection.RIGHT: origin.x += halfScale; break;
+                case FaceDirection.BACK: origin.z -= halfScale; break;
+                case FaceDirection.FRONT: origin.z += halfScale; break;
+                }
+
+                min[0] = Mathf.RoundToInt(Mathf.Min(min[0], origin.x));
+                max[0] = Mathf.RoundToInt(Mathf.Max(max[0], origin.x));
+
+                min[1] = Mathf.RoundToInt(Mathf.Min(min[1], origin.y));
+                max[1] = Mathf.RoundToInt(Mathf.Max(max[1], origin.y));
+
+                min[2] = Mathf.RoundToInt(Mathf.Min(min[2], origin.z));
+                max[2] = Mathf.RoundToInt(Mathf.Max(max[2], origin.z));
+            }
+        }
+
+
+        Debug.Log($"Need array of size: {(max[0] - min[0]) * (max[1] - min[1]) * (max[2] - min[2])}");
+
+        bool[,,] faces = new bool[max[0] - min[0] + 1,max[1] - min[1] + 1,max[2] - min[2] + 1];
+        foreach (var entry in voxelMap) {
+            ProgressMonitor.Worked(1);
+            if (NeedsYield(yieldTimer)) {
+                yield return null;
+            }
+
+            Vector3 origin = entry.Key;
+
+            foreach (FaceDirection face in directions) {
+                switch (face) {
+                case FaceDirection.TOP: origin.y -= halfScale; break;
+                case FaceDirection.BOTTOM: origin.y += halfScale; break;
+                case FaceDirection.LEFT: origin.x -= halfScale; break;
+                case FaceDirection.RIGHT: origin.x += halfScale; break;
+                case FaceDirection.BACK: origin.z -= halfScale; break;
+                case FaceDirection.FRONT: origin.z += halfScale; break;
+                }
+
+                int x = Mathf.RoundToInt(origin.x) - min[0];
+                int y = Mathf.RoundToInt(origin.y) - min[1];
+                int z = Mathf.RoundToInt(origin.z) - min[2];
+
+                if (x < 0 || y < 0 || z < 0 ||
+                    x >= faces.GetLength(0) ||
+                    y >= faces.GetLength(1) ||
+                    z >= faces.GetLength(2)) {
+                    Debug.Log($"{x}-{y}-{z} vs {faces.GetLength(0)}-{faces.GetLength(1)}-{faces.GetLength(2)}");
+                }
+                faces[x, y, z] = !faces[x, y, z];
+            }
+        }
+
+        ProgressMonitor.BeginSubtask("Generating Faces", voxelMap.Count);
+        var vertices = new Dictionary<Vector3, int>();
+        var colors = new List<Color>();
+        var triangles = new List<int>();
+
+        foreach (var entry in voxelMap) {
+            ProgressMonitor.Worked(1);
+            if (NeedsYield(yieldTimer)) {
+                yield return null;
+            }
+
+            Vector3 origin = entry.Key;
+
+            foreach (FaceDirection dir in directions) {
+                switch (dir) {
+                case FaceDirection.TOP: origin.y -= halfScale; break;
+                case FaceDirection.BOTTOM: origin.y += halfScale; break;
+                case FaceDirection.LEFT: origin.x -= halfScale; break;
+                case FaceDirection.RIGHT: origin.x += halfScale; break;
+                case FaceDirection.BACK: origin.z -= halfScale; break;
+                case FaceDirection.FRONT: origin.z += halfScale; break;
+                }
+
+                int x = Mathf.RoundToInt(origin.x) - min[0];
+                int y = Mathf.RoundToInt(origin.y) - min[1];
+                int z = Mathf.RoundToInt(origin.z) - min[2];
+
+                if (faces[x, y, z]) {
+                    Face face = new Face(origin, dir, halfScale);
+
+                    int a = vertices.AddIfAbsent(face.a, vertices.Count);
+                    int b = vertices.AddIfAbsent(face.b, vertices.Count);
+                    int c = vertices.AddIfAbsent(face.c, vertices.Count);
+                    int d = vertices.AddIfAbsent(face.d, vertices.Count);
+
+                    if (colors.Count <= a) { colors.Add(face.color); }
+                    if (colors.Count <= b) { colors.Add(face.color); }
+                    if (colors.Count <= c) { colors.Add(face.color); }
+                    if (colors.Count <= d) { colors.Add(face.color); }
+
+                    triangles.Add(a); triangles.Add(b); triangles.Add(c);
+                    triangles.Add(b); triangles.Add(d); triangles.Add(c);
+
+                    if (vertices.Count > 65534) {
+                        Debug.Log("Vertex limit hit!");
+                        BuildMesh(vertices, triangles, colors);
+
+                        vertices.Clear();
+                        triangles.Clear();
+                        colors.Clear();
+                    }
+                }
+            }
+        }
+
+        if (vertices.Count > 0) {
+            BuildMesh(vertices, triangles, colors);
+        }
+
+        /*
+        Dictionary<Vector3, Face> faces = new Dictionary<Vector3, Face>(voxelMap.Count * 6);
         foreach (var entry in voxelMap) {
             ProgressMonitor.Worked(1);
             if (NeedsYield(yieldTimer)) {
@@ -109,7 +237,7 @@ public class VoxelRenderer : MonoBehaviour {
                 yield return null;
             }
 
-            if (face.ignored) {
+            if (face.count != 1) {
                 continue;
             }
 
@@ -124,7 +252,7 @@ public class VoxelRenderer : MonoBehaviour {
             if (colors.Count <= b) { colors.Add(face.color); }
             if (colors.Count <= c) { colors.Add(face.color); }
             if (colors.Count <= d) { colors.Add(face.color); }
-            
+
             triangles.Add(a); triangles.Add(b); triangles.Add(c);
             triangles.Add(b); triangles.Add(d); triangles.Add(c);
 
@@ -142,12 +270,13 @@ public class VoxelRenderer : MonoBehaviour {
         if (vertices.Count > 0) {
             BuildMesh(vertices, triangles, colors);
         }
+        */
 
         // We're done!
         meshRenderer.enabled = true;
         ProgressMonitor.Finished();
 
-        Debug.Log($"Completed in {timer.ElapsedMilliseconds/1000.0f:F2}s");
+        Debug.Log($"Completed in {timer.ElapsedMilliseconds / 1000.0f:F2}s");
     }
 
     private void BuildMesh(IDictionary<Vector3, int> vertices, List<int> triangles, List<Color> colors) {
@@ -172,6 +301,8 @@ public class VoxelRenderer : MonoBehaviour {
         renderer.materials = meshRenderer.materials;
     }
 
+    int logged = 0;
+
     private void PopulateFace(IDictionary<Vector3, Face> faces, Vector3 origin, FaceDirection dir) {
         switch (dir) {
         case FaceDirection.TOP: origin.y -= halfScale; break;
@@ -182,11 +313,24 @@ public class VoxelRenderer : MonoBehaviour {
         case FaceDirection.FRONT: origin.z += halfScale; break;
         }
 
+        /*
         Face face = faces.GetOrDefault(origin);
-        if (face == null) {
+        face.count++;
+        if (face.count == 1) {
             faces[origin] = new Face(origin, dir, halfScale);
+        }
+        */
+
+        if (logged < 24) {
+            Debug.Log(origin);
+            ++logged;
+        }
+
+        Face face = null;
+        if (faces.TryGetValue(origin, out face)) {
+            face.count++;
         } else {
-            face.ignored = true;
+            //faces[origin] = new Face(origin, dir, halfScale);
         }
     }
 
@@ -201,25 +345,135 @@ public class VoxelRenderer : MonoBehaviour {
 
 
     private class Face {
-        public Vector3 a;
-        public Vector3 b;
-        public Vector3 c;
-        public Vector3 d;
+        public int count;
 
-        public bool ignored;
-        public Color color;
+        private float[] p = new float[5];
+        private FaceDirection dir;
+
+        public Vector3 a {
+            get {
+                switch (dir) {
+                case FaceDirection.TOP: return new Vector3(p[1], p[0], p[3]);
+                case FaceDirection.BOTTOM: return new Vector3(p[2], p[0], p[4]);
+
+                case FaceDirection.LEFT: return new Vector3(p[0], p[2], p[4]);
+                case FaceDirection.RIGHT: return new Vector3(p[0], p[1], p[3]);
+
+                case FaceDirection.FRONT: return new Vector3(p[2], p[4], p[0]);
+                case FaceDirection.BACK: return new Vector3(p[1], p[3], p[0]);
+
+                default: return Vector3.zero;
+                }
+            }
+        }
+
+        public Vector3 b {
+            get {
+                switch (dir) {
+                case FaceDirection.TOP: return new Vector3(p[2], p[0], p[3]);
+                case FaceDirection.BOTTOM: return new Vector3(p[2], p[0], p[3]);
+
+                case FaceDirection.LEFT: return new Vector3(p[0], p[2], p[3]);
+                case FaceDirection.RIGHT: return new Vector3(p[0], p[2], p[3]);
+
+                case FaceDirection.FRONT: return new Vector3(p[1], p[4], p[0]);
+                case FaceDirection.BACK: return new Vector3(p[1], p[4], p[0]);
+
+                default: return Vector3.zero;
+                }
+            }
+        }
+
+        public Vector3 c {
+            get {
+                switch (dir) {
+                case FaceDirection.TOP: return new Vector3(p[1], p[0], p[4]);
+                case FaceDirection.BOTTOM: return new Vector3(p[1], p[0], p[4]);
+
+                case FaceDirection.LEFT: return new Vector3(p[0], p[1], p[4]);
+                case FaceDirection.RIGHT: return new Vector3(p[0], p[1], p[4]);
+
+                case FaceDirection.FRONT: return new Vector3(p[2], p[3], p[0]);
+                case FaceDirection.BACK: return new Vector3(p[2], p[3], p[0]);
+
+                default: return Vector3.zero;
+                }
+            }
+        }
+
+        public Vector3 d {
+            get {
+                switch (dir) {
+                case FaceDirection.TOP: return new Vector3(p[2], p[0], p[2]);
+                case FaceDirection.BOTTOM: return new Vector3(p[1], p[0], p[3]);
+
+                case FaceDirection.LEFT: return new Vector3(p[0], p[1], p[3]);
+                case FaceDirection.RIGHT: return new Vector3(p[0], p[2], p[4]);
+
+                case FaceDirection.FRONT: return new Vector3(p[2], p[4], p[0]);
+                case FaceDirection.BACK: return new Vector3(p[2], p[4], p[0]);
+
+                default: return Vector3.zero;
+                }
+            }
+        }
+
+        public Color color {
+            get {
+                switch (dir) {
+                case FaceDirection.TOP: return Color.red;
+                case FaceDirection.BOTTOM: return Color.magenta;
+
+                case FaceDirection.LEFT: return Color.green;
+                case FaceDirection.RIGHT: return Color.yellow;
+
+                case FaceDirection.FRONT: return Color.blue;
+                case FaceDirection.BACK: return Color.cyan;
+
+                default: return Color.clear;
+                }
+            }
+        }
 
         public Face(Vector3 origin, FaceDirection dir, float halfScale) {
-            a = origin;
-            b = origin;
-            c = origin;
-            d = origin;
+            count = 1;
+            this.dir = dir;
 
             switch (dir) {
             case FaceDirection.TOP: // Correct
+            case FaceDirection.BOTTOM: // Correct
+                p[0] = origin.y;
+                p[1] = origin.x + halfScale;
+                p[2] = origin.x - halfScale;
+                p[3] = origin.z + halfScale;
+                p[4] = origin.z - halfScale;
+                break;
+
+            case FaceDirection.LEFT: // Correct
+            case FaceDirection.RIGHT:
+                p[0] = origin.x;
+                p[1] = origin.y + halfScale;
+                p[2] = origin.y - halfScale;
+                p[3] = origin.z + halfScale;
+                p[4] = origin.z - halfScale;
+                break;
+
+            case FaceDirection.FRONT:
+            case FaceDirection.BACK: // Correct
+                p[0] = origin.z;
+                p[1] = origin.x + halfScale;
+                p[2] = origin.x - halfScale;
+                p[3] = origin.y + halfScale;
+                p[4] = origin.y - halfScale;
+                break;
+            }
+
+            /*
+            switch (dir) {
+            case FaceDirection.TOP: // Correct
                 a.x += halfScale; a.z += halfScale;
-                c.x += halfScale; c.z -= halfScale;
                 b.x -= halfScale; b.z += halfScale;
+                c.x += halfScale; c.z -= halfScale;
                 d.x -= halfScale; d.z -= halfScale;
                 color = Color.red;
                 break;
@@ -243,8 +497,8 @@ public class VoxelRenderer : MonoBehaviour {
 
             case FaceDirection.RIGHT:
                 a.y += halfScale; a.z += halfScale;
-                c.y += halfScale; c.z -= halfScale;
                 b.y -= halfScale; b.z += halfScale;
+                c.y += halfScale; c.z -= halfScale;
                 d.y -= halfScale; d.z -= halfScale;
                 color = Color.yellow;
 
@@ -252,8 +506,8 @@ public class VoxelRenderer : MonoBehaviour {
 
             case FaceDirection.FRONT:
                 a.x -= halfScale; a.y -= halfScale;
-                c.x -= halfScale; c.y += halfScale;
                 b.x += halfScale; b.y -= halfScale;
+                c.x -= halfScale; c.y += halfScale;
                 d.x += halfScale; d.y += halfScale;
                 color = Color.blue;
 
@@ -267,7 +521,7 @@ public class VoxelRenderer : MonoBehaviour {
                 color = Color.cyan;
 
                 break;
-            }
+            }*/
         }
     }
 
